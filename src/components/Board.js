3c46abde0edc8c4364b8a9aedcca43a2e44f2c43
anton <kiyloanton@gmail.com>
2020-07-10 17:22:52 +0300

    created tic tac game

---
+++ src/components/Board.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import Square from './Square';
+
+function Board() {
+    const [state, setState] = useState({
+        squares: Array(9).fill(null),
+        xIsNext: true,
+    });
+
+    const winner = calculateWinner(state.squares);
+    
+    let status;
+    
+    if (winner) {
+        status = `WINNER: ${winner}`;
+    } else {
+        status = `NEXT PLAYER: ${state.xIsNext ? 'X' : 'O'}`;
+    };
+
+    function handleClick(i) {
+        const squares = state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        };
+
+        squares[i] = state.xIsNext ? 'X' : 'O';
+        setState({
+            squares: squares,
+            xIsNext: !state.xIsNext,
+        });
+    };
+
+    function renderSquare(i) {
+        return (
+            <Square
+                value={state.squares[i]}
+                onClick={() => handleClick(i)}
+            />
+        )
+    };
+
+    function calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            };
+        };
+    };
+
+    return (
+        <div>
+            <div className='status'>{status}</div>
+            <div className='board-row'>
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div className='board-row'>
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className='board-row'>
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+        </div>
+    );
+}
+
+export default Board;
